8036c514ff0b818e7395396b3b959ba5d42836df
zaypen <zaypen@gmail.com>
2018-04-28 17:28:58 +0800

    Add resolution of week 1

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -13,15 +13,44 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if (c == 0 || r == c) {
+      1
+    } else {
+      pascal(c - 1, r - 1) + pascal(c, r - 1)
+    }
+  }
   
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
-  
+  def balance(chars: List[Char]): Boolean = {
+    recursiveBalance(chars)
+  }
+
+  def recursiveBalance(chars: List[Char]): Boolean = {
+    val string = chars.filter(c => c == '(' || c == ')').mkString
+    val replaced = string.replace("()", "")
+    if (string == replaced) {
+      string.isEmpty
+    } else {
+      recursiveBalance(replaced.toList)
+    }
+  }
+
   /**
    * Exercise 3
    */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    val sorted = coins.sorted(Ordering[Int].reverse)
+    sorted.map(coin => {
+      if (money > coin) {
+        countChange(money - coin, sorted.dropWhile(c => c > coin))
+      } else if (money == coin) {
+        1
+      } else {
+        0
+      }
+    }).sum
   }
+}
